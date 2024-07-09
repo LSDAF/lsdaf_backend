c7207ad06af79386a7ce3cc6f413f94d7686c8c9
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-07-09 22:07:47 +0200

    [CONTROLLER ADVICE] Fixing error for BadRequestExceptionHandler

--- src/main/java/com/lsadf/lsadf_backend/controllers/advices/BadRequestExceptionHandler.java
+++ src/main/java/com/lsadf/lsadf_backend/controllers/advices/BadRequestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.lsadf.lsadf_backend.controllers.advices;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,9 +16,11 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class BadRequestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatus status,
-                                                                  final WebRequest request) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
         logger.error("400 Status Code", ex);
         return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
