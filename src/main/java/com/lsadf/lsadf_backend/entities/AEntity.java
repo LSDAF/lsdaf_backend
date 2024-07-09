d3ef95726814ae3966132efe802e1932231ff322
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-07-09 18:05:38 +0200

    [ENTITIES] Added entities package for JPA repo

---
+++ src/main/java/com/lsadf/lsadf_backend/entities/AEntity.java
@@ -0,0 +1,41 @@
+package com.lsadf.lsadf_backend.entities;
+
+import com.lsadf.lsadf_backend.constants.EntityAttributes;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+/**
+ * Abstact class for all database entities of KeyHab
+ */
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public abstract class AEntity {
+    private static final String UUID_HIBERNATE_GENERATOR = "uuid-hibernate-generator";
+    private static final String UUID_GENERATOR_STRATEGY = "org.hibernate.id.UUIDGenerator";
+
+    @Id
+    @GeneratedValue(generator = UUID_HIBERNATE_GENERATOR)
+    @Column(name = EntityAttributes.ID)
+    @GenericGenerator(name = UUID_HIBERNATE_GENERATOR, strategy = UUID_GENERATOR_STRATEGY)
+    @EqualsAndHashCode.Include
+    protected String id;
+
+    @Column(name = EntityAttributes.CREATED_AT, nullable = false, updatable = false)
+    @CreationTimestamp
+    protected Date createdAt;
+
+    @Column(name = EntityAttributes.UPDATED_AT)
+    @UpdateTimestamp
+    protected Date updatedAt;
+}
