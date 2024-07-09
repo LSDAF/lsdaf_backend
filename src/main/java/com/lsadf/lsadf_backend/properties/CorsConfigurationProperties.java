e401698c58a824d88086d1522ec49abd6bfe16ce
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-07-09 18:04:58 +0200

    [PROPERTIES] Added properties for project

---
+++ src/main/java/com/lsadf/lsadf_backend/properties/CorsConfigurationProperties.java
@@ -0,0 +1,18 @@
+package com.lsadf.lsadf_backend.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CorsConfigurationProperties {
+    private List<String> allowedOrigins;
+    private List<String> allowedMethods;
+    private List<String> allowedHeaders;
+    private List<String> exposedHeaders;
+    private Boolean allowCredentials;
+}
---
+++ src/main/java/com/lsadf/lsadf_backend/properties/SwaggerProperties.java
@@ -0,0 +1,15 @@
+package com.lsadf.lsadf_backend.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SwaggerProperties {
+    private String title;
+    private String description;
+    private String version;
+    private SwaggerContactProperties contact;
+}
