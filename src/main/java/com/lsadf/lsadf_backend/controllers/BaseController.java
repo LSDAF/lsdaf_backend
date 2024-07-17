45a8ac13b58e11e03a34574496d05d5f3da647a5
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-07-17 17:28:45 +0200

    [CONTROLLER] Created basecontroller for common method to validate user

---
+++ src/main/java/com/lsadf/lsadf_backend/controllers/BaseController.java
@@ -0,0 +1,28 @@
+package com.lsadf.lsadf_backend.controllers;
+
+import com.lsadf.lsadf_backend.exceptions.UnauthorizedException;
+import com.lsadf.lsadf_backend.models.LocalUser;
+import org.slf4j.Logger;
+
+public abstract class BaseController {
+
+    /**
+     * Gets the controller logger
+     * @return the logger
+     */
+    protected abstract Logger getLogger();
+
+    /**
+     * Validates the user
+     *
+     * @param localUser the user to validate
+     * @throws UnauthorizedException if the user is not valid
+     */
+    protected void validateUser(LocalUser localUser) throws UnauthorizedException {
+        if (localUser == null) {
+            Logger logger = getLogger();
+            logger.error("Unauthorized. Didn't manage to build UserInfo from token. Please login");
+            throw new UnauthorizedException("Unauthorized. Didn't manage to build UserInfo from token. Please login");
+        }
+    }
+}
