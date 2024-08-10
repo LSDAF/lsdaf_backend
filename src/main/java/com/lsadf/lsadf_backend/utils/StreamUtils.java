27cd29822f87addebbf22fd36977ee9824f6121a
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-08-10 11:51:34 +0200

    [STREAM UTILS] Updated StreamUtils

--- src/main/java/com/lsadf/lsadf_backend/utils/StreamUtils.java
+++ src/main/java/com/lsadf/lsadf_backend/utils/StreamUtils.java
@@ -2,22 +2,17 @@ package com.lsadf.lsadf_backend.utils;
 
 import com.lsadf.lsadf_backend.entities.GameSaveEntity;
 import com.lsadf.lsadf_backend.entities.UserEntity;
-import com.lsadf.lsadf_backend.requests.GameSaveOrderBy;
-import com.lsadf.lsadf_backend.requests.UserOrderBy;
+import com.lsadf.lsadf_backend.requests.game_save.GameSaveOrderBy;
+import com.lsadf.lsadf_backend.requests.user.UserOrderBy;
 import lombok.experimental.UtilityClass;
 
 import java.util.Comparator;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 @UtilityClass
 public class StreamUtils {
-    public static Stream<UserEntity> sortUsers(Stream<UserEntity> userStream, Optional<UserOrderBy> orderBy) {
-        if (orderBy.isEmpty()) {
-            return userStream;
-        }
-        UserOrderBy userOrderBy = orderBy.get();
-        return switch (userOrderBy) {
+    public static Stream<UserEntity> sortUsers(Stream<UserEntity> userStream, UserOrderBy orderBy) {
+        return switch (orderBy) {
             case ID -> userStream.sorted(Comparator.comparing(UserEntity::getId));
             case ID_DESC -> userStream.sorted(Comparator.comparing(UserEntity::getId).reversed());
             case EMAIL -> userStream.sorted(Comparator.comparing(UserEntity::getEmail));
@@ -28,21 +23,21 @@ public class StreamUtils {
             case CREATED_AT_DESC -> userStream.sorted(Comparator.comparing(UserEntity::getCreatedAt).reversed());
             case UPDATED_AT -> userStream.sorted(Comparator.comparing(UserEntity::getUpdatedAt));
             case UPDATED_AT_DESC -> userStream.sorted(Comparator.comparing(UserEntity::getUpdatedAt).reversed());
+            case NONE -> userStream;
         };
     }
 
-    public static Stream<GameSaveEntity> sortGameSaves(Stream<GameSaveEntity> gameSaveStream, Optional<GameSaveOrderBy> orderBy) {
-        if (orderBy.isEmpty()) {
-            return gameSaveStream;
-        }
-        GameSaveOrderBy gameSaveOrderBy = orderBy.get();
-        return switch (gameSaveOrderBy) {
+    public static Stream<GameSaveEntity> sortGameSaves(Stream<GameSaveEntity> gameSaveStream, GameSaveOrderBy orderBy) {
+        return switch (orderBy) {
             case CREATED_AT -> gameSaveStream.sorted(Comparator.comparing(GameSaveEntity::getCreatedAt));
             case CREATED_AT_DESC -> gameSaveStream.sorted(Comparator.comparing(GameSaveEntity::getCreatedAt).reversed());
             case UPDATED_AT -> gameSaveStream.sorted(Comparator.comparing(GameSaveEntity::getUpdatedAt));
             case UPDATED_AT_DESC -> gameSaveStream.sorted(Comparator.comparing(GameSaveEntity::getUpdatedAt).reversed());
             case ID -> gameSaveStream.sorted(Comparator.comparing(GameSaveEntity::getId));
             case ID_DESC -> gameSaveStream.sorted(Comparator.comparing(GameSaveEntity::getId).reversed());
+            case GOLD -> gameSaveStream.sorted(Comparator.comparing(GameSaveEntity::getGold));
+            case GOLD_DESC -> gameSaveStream.sorted(Comparator.comparing(GameSaveEntity::getGold).reversed());
+            case NONE -> gameSaveStream;
         };
     }
 }
