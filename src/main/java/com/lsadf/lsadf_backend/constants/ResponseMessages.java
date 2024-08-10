e3037a0c5d5b733ddfbaeea97f3034575fe80ac6
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-08-10 11:52:42 +0200

    [RESPONSE MESSSAGES] Updated new response for bad requests

--- src/main/java/com/lsadf/lsadf_backend/constants/ResponseMessages.java
+++ src/main/java/com/lsadf/lsadf_backend/constants/ResponseMessages.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ResponseMessages {
     public static final String OK = "OK";
+    public static final String BAD_REQUEST = "Bad Request - The request was invalid";
     public static final String UNAUTHORIZED = "Unauthorized - Authentication credentials were missing or incorrect.";
     public static final String FORBIDDEN = "Forbidden - The user does not have permission to perform this operation";
     public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
