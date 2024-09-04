d3ab7b66aa5359e2cee62331ce1c20f0fac05f2f
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-09-04 16:57:20 +0200

    [REDIS] Created RedisConstants for types of objects to save into redis

---
+++ src/main/java/com/lsadf/lsadf_backend/constants/RedisConstants.java
@@ -0,0 +1,13 @@
+package com.lsadf.lsadf_backend.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * Redis constants class
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class RedisConstants {
+    public static final String GOLD = "gold:";
+    public static final String GAME_SAVE_OWNERSHIP = "gamesaveownership:";
+}
