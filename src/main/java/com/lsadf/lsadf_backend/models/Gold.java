052d8963fc0eee034aa865413ab4da6a67e97b62
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-09-14 18:22:14 +0200

    [GOLD] Updated Gold POJO

--- src/main/java/com/lsadf/lsadf_backend/models/Gold.java
+++ src/main/java/com/lsadf/lsadf_backend/models/Gold.java
@@ -2,6 +2,7 @@ package com.lsadf.lsadf_backend.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.lsadf.lsadf_backend.constants.JsonAttributes;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
@@ -10,16 +11,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import static com.lsadf.lsadf_backend.constants.JsonAttributes.Gold.AMOUNT;
+import static com.lsadf.lsadf_backend.constants.JsonAttributes.Gold.ID;
 
 @Data
 @Schema(name = "Gold", description = "Gold amount Object")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonPropertyOrder({AMOUNT})
+@JsonPropertyOrder({ID, AMOUNT})
 public class Gold {
+    @JsonProperty(value = ID)
+    @Schema(description = "Id of the game save", example = "9cbfec92-d7ab-496d-adbd-f55fc14d8e8c")
+    private String id;
+
     @JsonProperty(value = AMOUNT)
     @PositiveOrZero
     @Schema(description = "Gold amount", example = "26093")
-    private long amount;
+    private Long amount;
 }
