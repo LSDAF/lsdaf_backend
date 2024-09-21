b73881d11ec4a1b5435004b8a782e1e25b1c1b0e
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-09-21 13:59:12 +0200

    [GLOBAL INFO] Added now property to globalInfo

--- src/main/java/com/lsadf/lsadf_backend/models/admin/GlobalInfo.java
+++ src/main/java/com/lsadf/lsadf_backend/models/admin/GlobalInfo.java
@@ -5,11 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class GlobalInfo {
+    private Instant now;
     private Long gameSaveCounter;
     private Long userCounter;
 }
