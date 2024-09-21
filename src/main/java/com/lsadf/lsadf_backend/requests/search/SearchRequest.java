ef776290c2e999039aa9b4e13bf3869ba2662297
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-09-21 11:51:28 +0200

    [REQUESTS] Updated all requests, added @JsonProperty to every properties

--- src/main/java/com/lsadf/lsadf_backend/requests/search/SearchRequest.java
+++ src/main/java/com/lsadf/lsadf_backend/requests/search/SearchRequest.java
@@ -1,5 +1,6 @@
 package com.lsadf.lsadf_backend.requests.search;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lsadf.lsadf_backend.requests.common.Filter;
 import com.lsadf.lsadf_backend.requests.common.OrderBy;
 import lombok.AllArgsConstructor;
@@ -8,9 +9,12 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+import static com.lsadf.lsadf_backend.constants.JsonAttributes.SearchRequest.FILTERS;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchRequest {
+    @JsonProperty(value = FILTERS)
     private List<Filter> filters;
 }
