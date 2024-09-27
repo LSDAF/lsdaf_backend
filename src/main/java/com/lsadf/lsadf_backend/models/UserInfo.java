ab782dff757b4853da149c63dfaf0418806e928a
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-09-28 00:18:42 +0200

    [MODEL] Added verified filed to UserInfo

--- src/main/java/com/lsadf/lsadf_backend/models/UserInfo.java
+++ src/main/java/com/lsadf/lsadf_backend/models/UserInfo.java
@@ -1,15 +1,14 @@
 package com.lsadf.lsadf_backend.models;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.lsadf.lsadf_backend.constants.JsonAttributes;
 import com.lsadf.lsadf_backend.constants.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import static com.lsadf.lsadf_backend.constants.JsonAttributes.CREATED_AT;
 import static com.lsadf.lsadf_backend.constants.JsonAttributes.UPDATED_AT;
@@ -17,7 +16,7 @@ import static com.lsadf.lsadf_backend.constants.JsonAttributes.UserInfo.*;
 
 @Data
 @AllArgsConstructor
-@JsonPropertyOrder({ID, NAME, EMAIL, ROLES, CREATED_AT, UPDATED_AT})
+@JsonPropertyOrder({ID, NAME, EMAIL, VERIFIED, ROLES, CREATED_AT, UPDATED_AT})
 public class UserInfo {
 
     @JsonProperty(value = ID)
@@ -29,8 +28,11 @@ public class UserInfo {
     @JsonProperty(value = EMAIL)
     private String email;
 
+    @JsonProperty(value = VERIFIED)
+    private boolean verified;
+
     @JsonProperty(value = ROLES)
-    private List<UserRole> roles;
+    private Set<UserRole> roles;
 
     @JsonProperty(value = CREATED_AT)
     private Date createdAt;
