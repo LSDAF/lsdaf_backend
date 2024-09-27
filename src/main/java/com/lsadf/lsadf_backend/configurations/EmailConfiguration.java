e0b16c374eb40e692f4c0785104b52f505c32047
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-09-27 22:04:57 +0200

    [CONFIGURATION] Added new config for Velocity + email configuration

---
+++ src/main/java/com/lsadf/lsadf_backend/configurations/EmailConfiguration.java
@@ -0,0 +1,36 @@
+package com.lsadf.lsadf_backend.configurations;
+
+import com.lsadf.lsadf_backend.properties.EmailProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+/**
+ * Configuration for email
+ */
+@Configuration
+public class EmailConfiguration {
+
+    @Bean
+    public JavaMailSender javaMailSender(EmailProperties emailProperties) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(emailProperties.getHost());
+        mailSender.setPort(emailProperties.getPort());
+        mailSender.setUsername(emailProperties.getUsername());
+        mailSender.setPassword(emailProperties.getPassword());
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", emailProperties.isAuth());
+        props.put("mail.smtp.starttls.enable", emailProperties.isStarttlsEnable());
+        props.put("mail.smtp.starttls.required", emailProperties.isStarttlsRequired());
+        props.put("mail.debug", emailProperties.isDebug());
+        props.put("mail.smtp.connectiontimeout", emailProperties.getConnectionTimeout());
+        props.put("mail.smtp.timeout", emailProperties.getTimeout());
+        props.put("mail.smtp.writetimeout", emailProperties.getWriteTimeout());
+
+        return mailSender;
+    }
+}
