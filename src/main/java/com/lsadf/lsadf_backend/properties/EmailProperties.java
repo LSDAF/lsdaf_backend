f7c287c0ce3002a79e5a173ad4d441bea812c470
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-09-27 22:03:56 +0200

    [PROPERTIES] Added new properties classes

---
+++ src/main/java/com/lsadf/lsadf_backend/properties/EmailProperties.java
@@ -0,0 +1,21 @@
+package com.lsadf.lsadf_backend.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailProperties {
+    private String host;
+    private boolean debug;
+    private int port;
+    private String username;
+    private String password;
+    private boolean auth;
+    private boolean starttlsEnabled;
+    private int connectionTimeout;
+    private int timeout;
+    private int writeTimeout;
+}
