f29ba075cc7b7486db30ac8b7826d19abdd48c38
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-10-23 23:10:45 +0200

    chore: added constructor to NotFoundException

--- src/main/java/com/lsadf/lsadf_backend/exceptions/http/NotFoundException.java
+++ src/main/java/com/lsadf/lsadf_backend/exceptions/http/NotFoundException.java
@@ -4,6 +4,10 @@ package com.lsadf.lsadf_backend.exceptions.http;
  * Exception for when a resource is not found.
  */
 public class NotFoundException extends RuntimeException {
+    public NotFoundException() {
+        super();
+    }
+
     public NotFoundException(String message) {
         super(message);
     }
