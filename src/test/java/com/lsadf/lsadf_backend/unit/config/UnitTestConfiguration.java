b48072654f12a8f9f168920617acceca511a3834
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-10-28 13:47:18 +0100

    chore: enriched UnitTestConfiguration, added mock beans

--- src/test/java/com/lsadf/lsadf_backend/unit/config/UnitTestConfiguration.java
+++ src/test/java/com/lsadf/lsadf_backend/unit/config/UnitTestConfiguration.java
@@ -5,13 +5,14 @@ import com.lsadf.lsadf_backend.configurations.ShutdownListener;
 import com.lsadf.lsadf_backend.http_clients.KeycloakAdminClient;
 import com.lsadf.lsadf_backend.http_clients.KeycloakClient;
 import com.lsadf.lsadf_backend.mappers.Mapper;
-import com.lsadf.lsadf_backend.services.CurrencyService;
-import com.lsadf.lsadf_backend.services.GameSaveService;
-import com.lsadf.lsadf_backend.services.StageService;
+import com.lsadf.lsadf_backend.services.*;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+import static com.lsadf.lsadf_backend.constants.BeanConstants.Service.REDIS_CACHE_SERVICE;
 
 @TestConfiguration
 public class UnitTestConfiguration {
@@ -40,8 +41,21 @@ public class UnitTestConfiguration {
     private StageService stageService;
 
     @MockBean
+    private UserService userService;
+
+    @MockBean
+    private SearchService searchService;
+
+    @MockBean
     private GameSaveService gameSaveService;
 
     @MockBean
     private CurrencyService currencyService;
+
+    @MockBean
+    @Qualifier(REDIS_CACHE_SERVICE)
+    private CacheService redisCacheService;
+
+    @MockBean
+    private CacheFlushService cacheFlushService;
 }
