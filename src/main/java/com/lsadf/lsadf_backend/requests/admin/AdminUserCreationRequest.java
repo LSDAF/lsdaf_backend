0621c8c6b5cd776c758d2911f81b7e5f0b3c6f2d
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-10-28 13:49:20 +0100

    chore: Updated admin request POJOs

--- src/main/java/com/lsadf/lsadf_backend/requests/admin/AdminUserCreationRequest.java
+++ src/main/java/com/lsadf/lsadf_backend/requests/admin/AdminUserCreationRequest.java
@@ -6,9 +6,11 @@ import com.lsadf.lsadf_backend.requests.Request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serial;
 
@@ -17,6 +19,7 @@ import static com.lsadf.lsadf_backend.constants.JsonAttributes.User.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder
 public class AdminUserCreationRequest implements Request {
 
     @Serial
@@ -32,16 +35,14 @@ public class AdminUserCreationRequest implements Request {
     @JsonProperty(value = LAST_NAME)
     private String lastName;
 
-    @Schema(description = "User id of user to create. Can be null", example = "7d9f92ce-3c8e-4695-9df7-ce10c0bbaaeb")
-    @JsonProperty(value = USER_ID)
-    private String userId;
-
     @Schema(description = "Enabled status of user to create", example = "true")
     @JsonProperty(value = ENABLED)
+    @NotNull
     private Boolean enabled;
 
     @Schema(description = "Verified email status of user to create", example = "true")
     @JsonProperty(value = EMAIL_VERIFIED)
+    @NotNull
     private Boolean emailVerified;
 
     @Email
