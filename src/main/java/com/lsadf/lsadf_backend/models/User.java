b43707cb35739bbf7109cd387d1522dd53c70978
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-11-01 19:57:18 +0100

    chore: updated models

--- src/main/java/com/lsadf/lsadf_backend/models/User.java
+++ src/main/java/com/lsadf/lsadf_backend/models/User.java
@@ -1,9 +1,10 @@
 package com.lsadf.lsadf_backend.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.lsadf.lsadf_backend.constants.JsonViews;
 import com.lsadf.lsadf_backend.serializers.DateToLongSerializer;
 import com.lsadf.lsadf_backend.serializers.LongToDateSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,7 +16,7 @@ import java.io.Serial;
 import java.util.Date;
 import java.util.List;
 
-import static com.lsadf.lsadf_backend.constants.JsonAttributes.*;
+import static com.lsadf.lsadf_backend.constants.JsonAttributes.ID;
 import static com.lsadf.lsadf_backend.constants.JsonAttributes.User.*;
 
 /**
@@ -24,40 +25,47 @@ import static com.lsadf.lsadf_backend.constants.JsonAttributes.User.*;
 @SuperBuilder
 @Data
 @AllArgsConstructor
-@JsonPropertyOrder({ID, FIRST_NAME, LAST_NAME, USERNAME, ENABLED, EMAIL_VERIFIED, USER_ROLES, CREATED_TIMESTAMP})
 public class User implements Model {
 
     @Serial
     private static final long serialVersionUID = 144315795668992686L;
 
+    @JsonView(JsonViews.Admin.class)
     @JsonProperty(value = ID)
     @Schema(description = "User Id", example = "7d9f92ce-3c8e-4695-9df7-ce10c0bbaaeb")
     private final String id;
 
+    @JsonView(JsonViews.Internal.class)
     @JsonProperty(value = FIRST_NAME)
     @Schema(description = "First name", example = "Toto")
     private String firstName;
 
+    @JsonView(JsonViews.Internal.class)
     @JsonProperty(value = LAST_NAME)
     @Schema(description = "Last name", example = "TUTU")
     private String lastName;
 
+    @JsonView(JsonViews.Internal.class)
     @JsonProperty(value = USERNAME)
     @Schema(description = "User username", example = "toto@toto.com")
     private final String username;
 
+    @JsonView(JsonViews.Internal.class)
     @JsonProperty(value = ENABLED)
     @Schema(description = "User enabled", example = "true")
     private boolean enabled;
 
+    @JsonView(JsonViews.Internal.class)
     @JsonProperty(value = EMAIL_VERIFIED)
     @Schema(description = "Email verified", example = "true")
     private boolean emailVerified;
 
+    @JsonView(JsonViews.Admin.class)
     @JsonProperty(value = USER_ROLES)
     @Schema(description = "User roles", example = "[\"USER\"]")
     private List<String> userRoles;
 
+    @JsonView(JsonViews.Admin.class)
     @JsonProperty(value = CREATED_TIMESTAMP)
     @Schema(description = "Creation date", example = "2022-01-01 00:00:00.000")
     @JsonSerialize(using = DateToLongSerializer.class)
--- src/main/java/com/lsadf/lsadf_backend/models/UserInfo.java
+++ src/main/java/com/lsadf/lsadf_backend/models/UserInfo.java
@@ -2,16 +2,12 @@ package com.lsadf.lsadf_backend.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.lsadf.lsadf_backend.constants.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serial;
-import java.util.Date;
 import java.util.Set;
 
-import static com.lsadf.lsadf_backend.constants.JsonAttributes.CREATED_AT;
-import static com.lsadf.lsadf_backend.constants.JsonAttributes.UPDATED_AT;
 import static com.lsadf.lsadf_backend.constants.JsonAttributes.UserInfo.*;
 
 @Data
