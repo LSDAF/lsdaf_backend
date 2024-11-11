1e87ff540a30e8e16bd5166e96737ea76bf3b313
adufou <antoine.du.fou@gmail.com>
2024-11-11 21:39:02 +0100

    feat: add first unit tests for inventory service

--- lsadf_api/src/main/java/com/lsadf/lsadf_backend/services/impl/InventoryServiceImpl.java
+++ lsadf_api/src/main/java/com/lsadf/lsadf_backend/services/impl/InventoryServiceImpl.java
@@ -1,12 +1,15 @@
 package com.lsadf.lsadf_backend.services.impl;
 
 import com.lsadf.lsadf_backend.cache.Cache;
+import com.lsadf.lsadf_backend.entities.InventoryEntity;
 import com.lsadf.lsadf_backend.exceptions.http.NotFoundException;
 import com.lsadf.lsadf_backend.mappers.Mapper;
 import com.lsadf.lsadf_backend.models.Inventory;
 import com.lsadf.lsadf_backend.repositories.InventoryRepository;
 import com.lsadf.lsadf_backend.services.InventoryService;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public class InventoryServiceImpl implements InventoryService {
 
@@ -29,8 +32,19 @@ public class InventoryServiceImpl implements InventoryService {
             throw new IllegalArgumentException("Game save id cannot be null");
         }
 
-        // TODO: Implement
-        return null;
+        if (inventoryCache.isEnabled()) {
+            Optional<Inventory> optionalCachedInventory = inventoryCache.get(gameSaveId);
+            if (optionalCachedInventory.isPresent()) {
+                Inventory inventory = optionalCachedInventory.get();
+                if (inventory.getItems() != null) {
+                    return inventory;
+                }
+            }
+        }
+
+        InventoryEntity inventoryEntity = getInventoryEntity(gameSaveId);
+
+        return mapper.mapInventoryEntityToInventory(inventoryEntity);
     }
 
     @Override
@@ -41,5 +55,17 @@ public class InventoryServiceImpl implements InventoryService {
         }
 
         // TODO: Implement
+    }
+
+    /**
+     * Get the inventory entity in the database or throw an exception if not found
+     *
+     * @param gameSaveId the game save id
+     * @return the inventory entity
+     * @throws NotFoundException if the inventory entity is not found
+     */
+    private InventoryEntity getInventoryEntity(String gameSaveId) throws NotFoundException {
+        return inventoryRepository.findById(gameSaveId)
+                .orElseThrow(() -> new NotFoundException("Inventory not found for game save id " + gameSaveId));
     }
 }
--- lsadf_api/src/test/java/com/lsadf/lsadf_backend/unit/services/InventoryServiceTests.java
+++ lsadf_api/src/test/java/com/lsadf/lsadf_backend/unit/services/InventoryServiceTests.java
@@ -1,9 +1,106 @@
 package com.lsadf.lsadf_backend.unit.services;
 
+import com.lsadf.lsadf_backend.cache.Cache;
+import com.lsadf.lsadf_backend.entities.InventoryEntity;
+import com.lsadf.lsadf_backend.exceptions.http.NotFoundException;
+import com.lsadf.lsadf_backend.mappers.Mapper;
+import com.lsadf.lsadf_backend.mappers.impl.MapperImpl;
+import com.lsadf.lsadf_backend.models.Inventory;
+import com.lsadf.lsadf_backend.repositories.InventoryRepository;
+import com.lsadf.lsadf_backend.services.InventoryService;
+import com.lsadf.lsadf_backend.services.impl.InventoryServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class InventoryServiceTests {
+    private InventoryService inventoryService;
 
+    @Mock
+    private InventoryRepository inventoryRepository;
+
+    @Mock
+    private Cache<Inventory> inventoryCache;
+
+    private final Mapper mapper = new MapperImpl();
+
+    @BeforeEach
+    public void init() {
+        // Create all mocks and inject them into the service
+        MockitoAnnotations.openMocks(this);
+
+        inventoryService = new InventoryServiceImpl(inventoryRepository, inventoryCache, mapper);
+    }
+
+    @Test
+    void getInventory_on_null_gamesave_id() {
+        // Act & Assert
+        assertThrows(IllegalArgumentException.class, () -> inventoryService.getInventory(null));
+    }
+
+    @Test
+    void getInventory_on_non_existing_gamesave_id() {
+        // Arrange
+        when(inventoryRepository.findById(anyString())).thenReturn(Optional.empty());
+        when(inventoryCache.isEnabled()).thenReturn(true);
+
+        // Assert
+        assertThrows(NotFoundException.class, () -> inventoryService.getInventory("1"));
+    }
+
+    @Test
+    void getInventory_on_existing_gamesave_id_when_cached_empty() {
+        // Arrange
+        InventoryEntity inventoryEntity = InventoryEntity.builder()
+                .items(new ArrayList<>())
+                .build();
+
+        Inventory inventory = Inventory.builder()
+                .items(new ArrayList<>())
+                .build();
+
+        when(inventoryRepository.findById(anyString())).thenReturn(Optional.of(inventoryEntity));
+        when(inventoryCache.isEnabled()).thenReturn(true);
+        when(inventoryCache.get(anyString())).thenReturn(Optional.of(inventory));
+
+        // Act
+        Inventory result = inventoryService.getInventory("1");
+
+        // Assert
+        assertThat(result).isEqualTo(inventory);
+    }
+
+    @Test
+    void getInventory_on_existing_gamesave_id_when_not_cached_empty() {
+        // Arrange
+        InventoryEntity inventoryEntity = InventoryEntity.builder()
+                .items(new ArrayList<>())
+                .build();
+
+        Inventory inventory = Inventory.builder()
+                .items(new ArrayList<>())
+                .build();
+
+        when(inventoryRepository.findById(anyString())).thenReturn(Optional.of(inventoryEntity));
+        when(inventoryCache.isEnabled()).thenReturn(false);
+        when(inventoryCache.get(anyString())).thenReturn(Optional.of(inventory));
+
+        // Act
+        Inventory result = inventoryService.getInventory("1");
+
+        // Assert
+        assertThat(result).isEqualTo(inventory);
+    }
 }
