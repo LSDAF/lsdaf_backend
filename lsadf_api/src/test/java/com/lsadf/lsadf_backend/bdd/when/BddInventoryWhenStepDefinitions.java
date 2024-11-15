68589e79af7bcde80d74b4a9515a78f30b4abdc6
adufou <antoine.du.fou@gmail.com>
2024-11-15 18:20:38 +0100

    feat: bdd tests for delete controller

--- lsadf_api/src/test/java/com/lsadf/lsadf_backend/bdd/when/BddInventoryWhenStepDefinitions.java
+++ lsadf_api/src/test/java/com/lsadf/lsadf_backend/bdd/when/BddInventoryWhenStepDefinitions.java
@@ -102,6 +102,50 @@ public class BddInventoryWhenStepDefinitions extends BddLoader {
         }
     }
 
+    @When("the user requests the endpoint to create an item in the inventory of the game save with id (.*) with the following ItemCreationRequest$")
+    public void when_the_user_requests_the_endpoint_to_create_an_item_in_the_inventory_of_the_game_save_with_id_with_the_following_item_creation_request(String gameSaveId, DataTable dataTable) {
+        List<Map<String, String>> rows = dataTable.asMaps(String.class, String.class);
+        assertThat(rows).hasSize(1);
+
+        Map<String, String> row = rows.get(0);
+        ItemRequest itemRequest = BddUtils.mapToItemRequest(row);
+
+        String fullPath = ControllerConstants.INVENTORY + ControllerConstants.Inventory.ITEMS.replace("{game_save_id}", gameSaveId);
+        String url = BddUtils.buildUrl(this.serverPort, fullPath);
+        try {
+            JwtAuthentication jwtAuthentication = jwtAuthenticationStack.peek();
+            String token = jwtAuthentication.getAccessToken();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(token);
+            HttpEntity<ItemRequest> request = new HttpEntity<>(itemRequest, headers);
+            ResponseEntity<GenericResponse<Void>> result = testRestTemplate.exchange(url, HttpMethod.POST, request, buildParameterizedVoidResponse());
+            GenericResponse<Void> body = result.getBody();
+            responseStack.push(body);
+            log.info("Response: {}", result);
+        } catch (Exception e) {
+            exceptionStack.push(e);
+        }
+    }
+
+    @When("the user requests the endpoint to delete an item with id (.*) in the inventory of the game save with id (.*)$")
+    public void when_the_user_requests_the_endpoint_to_delete_an_item_with_id_in_the_inventory_of_the_game_save_with_id(String itemId, String gameSaveId) {
+        String fullPath = ControllerConstants.INVENTORY + ControllerConstants.Inventory.ITEM_ID.replace("{game_save_id}", gameSaveId).replace("{item_id}", itemId);
+        String url = BddUtils.buildUrl(this.serverPort, fullPath);
+        try {
+            JwtAuthentication jwtAuthentication = jwtAuthenticationStack.peek();
+            String token = jwtAuthentication.getAccessToken();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(token);
+            HttpEntity<Void> request = new HttpEntity<>(headers);
+            ResponseEntity<GenericResponse<Void>> result = testRestTemplate.exchange(url, HttpMethod.DELETE, request, buildParameterizedVoidResponse());
+            GenericResponse<Void> body = result.getBody();
+            responseStack.push(body);
+            log.info("Response: {}", result);
+        } catch (Exception e) {
+            exceptionStack.push(e);
+        }
+    }
+
     @When("the user requests the endpoint to update an item with id (.*) in the inventory of the game save with id (.*) with the following ItemUpdateRequest$")
     public void when_the_user_requests_the_endpoint_to_update_an_item_in_the_inventory_of_the_game_save_with_id_with_the_following_item_update_request(String itemId, String gameSaveId, DataTable dataTable) {
         List<Map<String, String>> rows = dataTable.asMaps(String.class, String.class);
@@ -127,30 +171,6 @@ public class BddInventoryWhenStepDefinitions extends BddLoader {
         }
     }
 
-    @When("the user requests the endpoint to create an item in the inventory of the game save with id (.*) with the following ItemCreationRequest$")
-    public void when_the_user_requests_the_endpoint_to_create_an_item_in_the_inventory_of_the_game_save_with_id_with_the_following_item_creation_request(String gameSaveId, DataTable dataTable) {
-        List<Map<String, String>> rows = dataTable.asMaps(String.class, String.class);
-        assertThat(rows).hasSize(1);
-
-        Map<String, String> row = rows.get(0);
-        ItemRequest itemRequest = BddUtils.mapToItemRequest(row);
-
-        String fullPath = ControllerConstants.INVENTORY + ControllerConstants.Inventory.ITEMS.replace("{game_save_id}", gameSaveId);
-        String url = BddUtils.buildUrl(this.serverPort, fullPath);
-        try {
-            JwtAuthentication jwtAuthentication = jwtAuthenticationStack.peek();
-            String token = jwtAuthentication.getAccessToken();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setBearerAuth(token);
-            HttpEntity<ItemRequest> request = new HttpEntity<>(itemRequest, headers);
-            ResponseEntity<GenericResponse<Void>> result = testRestTemplate.exchange(url, HttpMethod.POST, request, buildParameterizedVoidResponse());
-            GenericResponse<Void> body = result.getBody();
-            responseStack.push(body);
-            log.info("Response: {}", result);
-        } catch (Exception e) {
-            exceptionStack.push(e);
-        }
-    }
 
     @Then("^the inventory of the game save with id (.*) should be empty$")
     public void when_the_inventory_of_the_game_save_with_id_should_be_empty(String gameSaveId) {
