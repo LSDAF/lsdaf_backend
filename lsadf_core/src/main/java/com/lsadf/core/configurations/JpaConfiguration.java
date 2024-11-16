66b75d212e1069b41ac5ef63dd34c778fc0d7c59
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-11-16 14:57:00 +0100

    fix!: fixed tests + made applications work with new jpa config to enable for repositories

---
+++ lsadf_core/src/main/java/com/lsadf/core/configurations/JpaConfiguration.java
@@ -0,0 +1,13 @@
+package com.lsadf.core.configurations;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@ConditionalOnProperty(name = "jpa.enabled", havingValue = "true")
+@EnableJpaRepositories(basePackages = "com.lsadf.core.repositories")
+@EntityScan(basePackages = "com.lsadf.core.entities")
+public class JpaConfiguration {
+}
