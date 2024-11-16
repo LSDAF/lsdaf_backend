77a34f22dd0277f4077cd1ebbeec58c436f9e925
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-11-16 14:57:00 +0100

    fix!: fixed tests + made applications work with new jpa config to enable for repositories

---
+++ lsadf_core/src/main/java/com/lsadf/core/properties/JpaProperties.java
@@ -0,0 +1,12 @@
+package com.lsadf.core.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JpaProperties {
+    private boolean enabled;
+}
