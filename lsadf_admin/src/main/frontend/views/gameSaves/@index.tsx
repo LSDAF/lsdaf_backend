670550353c751c983aa53c55be6804893b2d579e
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-11-26 12:52:03 +0100

    feat: updated new index pages for admin front

--- lsadf_admin/src/main/frontend/views/gameSaves/@index.tsx
+++ lsadf_admin/src/main/frontend/views/gameSaves/@index.tsx
@@ -1,14 +1,29 @@
-import {AutoCrud} from "@vaadin/hilla-react-crud";
 import {AdminGameSaveService} from "Frontend/generated/endpoints";
 import gameSaveModel from "Frontend/generated/com/lsadf/core/models/GameSaveModel";
 import Layout from "Frontend/views/layout";
+import React from "react";
+import {AutoGrid} from "@vaadin/hilla-react-crud";
+import {GridColumn} from "@vaadin/react-components";
+
+function gameSaveDetailsButtonRenderer({item}: { item: gameSaveModel }) {
+    return (
+        <a href={`/gameSaves/${item.id}`} className="text-primary">Details</a>
+    );
+}
 
 
 export default function GameSavesView() {
-    // @ts-ignore
     return (
         <Layout>
-            <AutoCrud service={AdminGameSaveService} model={gameSaveModel} />
+            {/*<AutoCrud service={AdminGameSaveService} model={gameSaveModel} />*/}
+            <AutoGrid
+                service={AdminGameSaveService}
+                model={gameSaveModel}
+                visibleColumns={['id', 'user_email', 'created_at', 'updated_at', 'nickname']}
+                customColumns={[
+                    <GridColumn key="details" header="Details" renderer={gameSaveDetailsButtonRenderer}/>
+                ]}
+            />
         </Layout>
     )
 
--- lsadf_admin/src/main/frontend/views/users/@index.tsx
+++ lsadf_admin/src/main/frontend/views/users/@index.tsx
@@ -1,13 +1,12 @@
-import {AutoCrud} from "@vaadin/hilla-react-crud";
 import {AdminUserService} from "Frontend/generated/endpoints";
 import UserModel from "Frontend/generated/com/lsadf/core/models/UserModel";
 import Layout from "Frontend/views/layout";
+import {AutoGrid} from "@vaadin/hilla-react-crud";
 
 export default function UsersView() {
-    // @ts-ignore
     return (
         <Layout>
-            <AutoCrud service={AdminUserService} model={UserModel} />
+            <AutoGrid service={AdminUserService} model={UserModel} />
         </Layout>
     );
 }
