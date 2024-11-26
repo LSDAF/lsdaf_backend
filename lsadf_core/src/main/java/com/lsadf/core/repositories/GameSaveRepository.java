046d72fa0cebbab95a1c9a29bb49629850695089
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-11-26 12:54:31 +0100

    fix: added new extension for repositories + fixing lint error

--- lsadf_core/src/main/java/com/lsadf/core/repositories/GameSaveRepository.java
+++ lsadf_core/src/main/java/com/lsadf/core/repositories/GameSaveRepository.java
@@ -1,20 +1,27 @@
 package com.lsadf.core.repositories;
 
 import com.lsadf.core.entities.GameSaveEntity;
-import java.util.Optional;
-import java.util.stream.Stream;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-/** Repository class for GameSaveEntity */
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Repository class for GameSaveEntity
+ */
 @Repository
-public interface GameSaveRepository extends CrudRepository<GameSaveEntity, String> {
-  @Query("select gs from t_game_save gs")
-  Stream<GameSaveEntity> findAllGameSaves();
+public interface GameSaveRepository extends
+        CrudRepository<GameSaveEntity, String>,
+        PagingAndSortingRepository<GameSaveEntity, String> {
+    @Query("select gs from t_game_save gs")
+    Stream<GameSaveEntity> findAllGameSaves();
 
-  Optional<GameSaveEntity> findGameSaveEntityByNickname(String nickname);
+    Optional<GameSaveEntity> findGameSaveEntityByNickname(String nickname);
 
-  @Query("select gs from t_game_save gs where gs.userEmail = :userId")
-  Stream<GameSaveEntity> findGameSaveEntitiesByUserEmail(String userId);
+    @Query("select gs from t_game_save gs where gs.userEmail = :userId")
+    Stream<GameSaveEntity> findGameSaveEntitiesByUserEmail(String userId);
+
 }
--- lsadf_core/src/main/java/com/lsadf/core/repositories/StageRepository.java
+++ lsadf_core/src/main/java/com/lsadf/core/repositories/StageRepository.java
@@ -2,7 +2,11 @@ package com.lsadf.core.repositories;
 
 import com.lsadf.core.entities.StageEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StageRepository extends CrudRepository<StageEntity, String> {}
+public interface StageRepository extends
+        CrudRepository<StageEntity, String>,
+        PagingAndSortingRepository<StageEntity, String> {
+}
