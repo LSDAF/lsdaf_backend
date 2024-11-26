dc9226eb5c1a8ae7d565e70d682e83b5bf26e446
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-11-26 15:51:23 +0100

    feat: printing id of game save if exists

--- lsadf_admin/src/main/frontend/views/gameSaves/{gameSaveId}.tsx
+++ lsadf_admin/src/main/frontend/views/gameSaves/{gameSaveId}.tsx
@@ -1,6 +1,5 @@
 import {useParams} from "react-router-dom";
 import {useSignal} from "@vaadin/hilla-react-signals";
-import GameSaveModel from "Frontend/generated/com/lsadf/core/models/GameSaveModel";
 import {useEffect} from "react";
 import {AdminGameSaveService} from "Frontend/generated/endpoints";
 import GameSave from "Frontend/generated/com/lsadf/core/models/GameSave";
@@ -16,4 +15,8 @@ export default function GameSaveView() {
     useEffect(() => {
         AdminGameSaveService.get(gameSaveId).then(p => gameSave.value = p);
     }, [gameSaveId]);
+
+    return (
+        <div>{gameSave.value?.id}</div>
+    )
 }
