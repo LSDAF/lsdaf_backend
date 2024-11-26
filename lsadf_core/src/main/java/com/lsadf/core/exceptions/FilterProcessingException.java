d5aa78ebb83e2c26d840c7f9af414ddcb233f749
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-11-26 12:40:14 +0100

    feat: added new filter utilities functions for streams

---
+++ lsadf_core/src/main/java/com/lsadf/core/exceptions/FilterProcessingException.java
@@ -0,0 +1,10 @@
+package com.lsadf.core.exceptions;
+
+/**
+ * Exception for when there is an error processing a filter
+ */
+public class FilterProcessingException extends RuntimeException {
+    public FilterProcessingException(String message) {
+        super(message);
+    }
+}
