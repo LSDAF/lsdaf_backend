603f72ddfb1f5faf799319022c3ac6705e6ea75f
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-11-26 12:55:38 +0100

    fix: updated admin services for frontend

--- lsadf_admin/src/main/java/com/lsadf/admin/services/AdminGameSaveService.java
+++ lsadf_admin/src/main/java/com/lsadf/admin/services/AdminGameSaveService.java
@@ -1,39 +1,67 @@
 package com.lsadf.admin.services;
 
+import com.lsadf.admin.utils.FilterUtils;
 import com.lsadf.core.mappers.Mapper;
 import com.lsadf.core.models.GameSave;
+import com.lsadf.core.requests.game_save.GameSaveSortingParameter;
 import com.lsadf.core.services.GameSaveService;
+import com.lsadf.core.utils.StreamUtils;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.hilla.BrowserCallable;
-import com.vaadin.hilla.crud.CrudService;
+import com.vaadin.hilla.Nonnull;
+import com.vaadin.hilla.Nullable;
+import com.vaadin.hilla.crud.ListService;
 import com.vaadin.hilla.crud.filter.Filter;
+import lombok.NonNull;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @BrowserCallable
 @AnonymousAllowed
-public class AdminGameSaveService implements CrudService<GameSave, String> {
+public class AdminGameSaveService implements ListService<GameSave> {
 
     private final GameSaveService gameSaveService;
     private final Mapper mapper;
 
-    public AdminGameSaveService(GameSaveService gameSaveService, Mapper mapper) {
+    public AdminGameSaveService(GameSaveService gameSaveService,
+                                Mapper mapper) {
         this.gameSaveService = gameSaveService;
         this.mapper = mapper;
     }
 
     @Override
-    public GameSave save(GameSave value) {
-        return null;
+    @Nonnull
+    @NonNull
+    public List<@Nonnull GameSave> list(@NonNull Pageable pageable,
+                               @jakarta.annotation.Nullable @Nullable Filter filter) {
+        Stream<GameSave> gameSaveStream = gameSaveService.getGameSaves()
+                .map(mapper::mapGameSaveEntityToGameSave);
+
+        // Filter the stream
+        if (filter != null) {
+            gameSaveStream = FilterUtils.applyFilters(gameSaveStream, filter);
+        }
+
+        // Sort the stream
+
+        // map pageable to a list of GameSaveSortingParameter
+        List<GameSaveSortingParameter> sortingParameters = pageable.getSort().stream()
+                .map(GameSaveSortingParameter::fromOrder)
+                .toList();
+
+        gameSaveStream = StreamUtils.sortGameSaves(gameSaveStream, sortingParameters);
+
+        // Paginate the stream
+        return gameSaveStream
+                .skip(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .toList();
     }
 
-    @Override
-    public void delete(String s) {
-    }
-
-    @Override
-    public List<GameSave> list(Pageable pageable, Filter filter) {
-        return List.of();
+    public GameSave get(@Nonnull String id) {
+        var gameSave = gameSaveService.getGameSave(id);
+        return mapper.mapGameSaveEntityToGameSave(gameSave);
     }
 }
--- lsadf_admin/src/main/java/com/lsadf/admin/services/AdminUserService.java
+++ lsadf_admin/src/main/java/com/lsadf/admin/services/AdminUserService.java
@@ -1,21 +1,26 @@
 package com.lsadf.admin.services;
 
+import com.lsadf.admin.utils.FilterUtils;
 import com.lsadf.core.mappers.Mapper;
 import com.lsadf.core.models.User;
+import com.lsadf.core.requests.user.UserSortingParameter;
 import com.lsadf.core.services.UserService;
+import com.lsadf.core.utils.StreamUtils;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.hilla.BrowserCallable;
 import com.vaadin.hilla.Nonnull;
 import com.vaadin.hilla.Nullable;
-import com.vaadin.hilla.crud.CrudService;
+import com.vaadin.hilla.crud.ListService;
 import com.vaadin.hilla.crud.filter.Filter;
+import lombok.NonNull;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @BrowserCallable
 @AnonymousAllowed
-public class AdminUserService implements CrudService<User, String> {
+public class AdminUserService implements ListService<User> {
     private final UserService userService;
     private final Mapper mapper;
 
@@ -25,17 +30,29 @@ public class AdminUserService implements CrudService<User, String> {
     }
 
     @Override
-    public @Nullable User save(User value) {
-        return value;
-    }
+    @NonNull
+    @Nonnull
+    public List<@Nonnull User> list(@NonNull Pageable pageable,
+                                    @jakarta.annotation.Nullable @Nullable Filter filter) {
+        Stream<User> userStream = userService.getUsers();
 
-    @Override
-    public void delete(String s) {
-        userService.deleteUser(s);
-    }
+        // Filter the stream
+        if (filter != null) {
+            userStream = FilterUtils.applyFilters(userStream, filter);
+        }
 
-    @Override
-    public @Nonnull List<@Nonnull User> list(Pageable pageable, @Nullable Filter filter) {
-        return List.of();
+        // map the pageable into a list of sorting parameters
+        List<UserSortingParameter> sortingParameters = pageable.getSort().stream()
+                .map(UserSortingParameter::fromOrder)
+                .toList();
+
+        // Sort the stream
+        userStream = StreamUtils.sortUsers(userStream, sortingParameters);
+
+        // Paginate the stream
+        return userStream
+                .skip(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .toList();
     }
 }
