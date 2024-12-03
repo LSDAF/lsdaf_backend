1716d6c488d8bb16f9589292d85afe3635ea892b
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-12-03 12:29:39 +0100

    feat WIP!: added new exposed service to get user info for admin UI

---
+++ lsadf_admin/src/main/java/com/lsadf/admin/services/AdminUserInfoService.java
@@ -0,0 +1,10 @@
+package com.lsadf.admin.services;
+
+import com.lsadf.core.models.UserInfo;
+
+/**
+ * Service for getting user info from the admin UI
+ */
+public interface AdminUserInfoService {
+    UserInfo getUserInfo();
+}
