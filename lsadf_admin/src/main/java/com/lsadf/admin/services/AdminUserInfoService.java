9a2863a483a05aa10762f2d39ea8e2d4daa9b661
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-12-03 12:41:10 +0100

    lint: linted code

--- lsadf_admin/src/main/java/com/lsadf/admin/services/AdminUserInfoService.java
+++ lsadf_admin/src/main/java/com/lsadf/admin/services/AdminUserInfoService.java
@@ -2,9 +2,7 @@ package com.lsadf.admin.services;
 
 import com.lsadf.core.models.UserInfo;
 
-/**
- * Service for getting user info from the admin UI
- */
+/** Service for getting user info from the admin UI */
 public interface AdminUserInfoService {
-    UserInfo getUserInfo();
+  UserInfo getUserInfo();
 }
