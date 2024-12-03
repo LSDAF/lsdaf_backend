ffdada4cbf324e7f18503fbede0185aff18cfdf9
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-12-03 12:30:18 +0100

    feat WIP!: refactored code

--- lsadf_api/src/main/java/com/lsadf/controllers/impl/UserControllerImpl.java
+++ lsadf_api/src/main/java/com/lsadf/controllers/impl/UserControllerImpl.java
@@ -7,15 +7,10 @@ import com.lsadf.controllers.UserController;
 import com.lsadf.core.controllers.impl.BaseController;
 import com.lsadf.core.models.UserInfo;
 import com.lsadf.core.responses.GenericResponse;
-import com.lsadf.core.utils.TokenUtils;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,14 +27,7 @@ public class UserControllerImpl extends BaseController implements UserController
   /** {@inheritDoc} */
   @Override
   public ResponseEntity<GenericResponse<UserInfo>> getUserInfo(Jwt jwt) {
-    String username = getUsernameFromJwt(jwt);
-    String name = getNameFromJwt(jwt);
-    boolean verified = getEmailVerifiedFromJwt(jwt);
-    List<GrantedAuthority> authorities = TokenUtils.getRolesFromJwt(jwt);
-    Set<String> roles =
-        authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
-
-    UserInfo userInfo = new UserInfo(name, username, verified, roles);
+    UserInfo userInfo = getUserInfoFromJwt(jwt);
     return generateResponse(HttpStatus.OK, userInfo);
   }
 }
