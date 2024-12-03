82de0a293b606495913505df9cb8ba07000ba3e6
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-12-03 12:33:24 +0100

    lint!: linted layout

--- lsadf_admin/src/main/frontend/views/@layout.tsx
+++ lsadf_admin/src/main/frontend/views/@layout.tsx
@@ -1,21 +1,15 @@
-import {
-    AppLayout,
-    DrawerToggle,
-    HorizontalLayout,
-    Icon, ProgressBar,
-    Scroller,
-    SideNav,
-    SideNavItem
-} from "@vaadin/react-components";
+import {AppLayout, DrawerToggle, Icon, ProgressBar, Scroller, SideNav, SideNavItem} from "@vaadin/react-components";
+import {initKeycloak} from "Frontend/keycloak";
 import {Suspense} from "react";
 import {Outlet} from "react-router-dom";
-import {initKeycloak} from "Frontend/keycloak";
 
 initKeycloak().then(() => {
     console.log("Keycloak initialized");
 });
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+
+export default function Layout({children}: { children: React.ReactNode }) {
+
     return (
         <AppLayout primarySection="drawer">
             <h1 slot="drawer">
@@ -24,19 +18,19 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <Scroller slot="drawer" className="p-s">
                 <SideNav>
                     <SideNavItem path="/users">
-                        <Icon icon="vaadin:user" slot="prefix" />
+                        <Icon icon="vaadin:user" slot="prefix"/>
                         Users
                     </SideNavItem>
                     <SideNavItem path="/gameSaves">
-                        <Icon icon="vaadin:gamepad" slot="prefix" />
+                        <Icon icon="vaadin:gamepad" slot="prefix"/>
                         Game Saves
                     </SideNavItem>
                 </SideNav>
             </Scroller>
 
             <div slot="navbar">
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <DrawerToggle />
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <DrawerToggle/>
                     <h2>Orders</h2>
                 </div>
 
@@ -58,8 +52,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 {/*</HorizontalLayout>*/}
             </div>
 
-            <Suspense fallback={<ProgressBar indeterminate={true} className="m-0" />}>
-                <Outlet />
+            <Suspense fallback={<ProgressBar indeterminate={true} className="m-0"/>}>
+                <Outlet/>
             </Suspense>
 
         </AppLayout>
