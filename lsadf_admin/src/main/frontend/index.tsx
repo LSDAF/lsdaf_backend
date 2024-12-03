8490d014024e2cc4bf6dc1b6593bf5cd380bb619
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-12-03 17:23:52 +0100

    feat: added index.tsx file for main class

---
+++ lsadf_admin/src/main/frontend/index.tsx
@@ -0,0 +1,14 @@
+import {RouterProvider} from "react-router-dom";
+import {router} from "Frontend/generated/routes";
+import {createRoot} from "react-dom/client";
+import {createElement} from "react";
+
+function App() {
+    return <RouterProvider router={router}/>;
+}
+
+const outlet = document.getElementById('outlet')!;
+let root = (outlet as any)._root ?? createRoot(outlet);
+(outlet as any)._root = root;
+root.render(createElement(App));
+
