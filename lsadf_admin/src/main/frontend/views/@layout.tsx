d56b951c2bed1d73eeffdb030b75e64a4242d415
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-12-05 10:23:45 +0100

    feat: added logout button to test keycloak logout mechanism

--- lsadf_admin/src/main/frontend/views/@layout.tsx
+++ lsadf_admin/src/main/frontend/views/@layout.tsx
@@ -1,5 +1,14 @@
-import {AppLayout, DrawerToggle, Icon, ProgressBar, Scroller, SideNav, SideNavItem} from "@vaadin/react-components";
-import {initKeycloak} from "Frontend/keycloak";
+import {
+    AppLayout,
+    Button,
+    DrawerToggle,
+    Icon,
+    ProgressBar,
+    Scroller,
+    SideNav,
+    SideNavItem
+} from "@vaadin/react-components";
+import keycloak, {initKeycloak} from "Frontend/keycloak";
 import {Suspense} from "react";
 import {Outlet} from "react-router-dom";
 import {Toaster} from "react-hot-toast";
@@ -8,8 +17,21 @@ initKeycloak().then(() => {
     console.log("Keycloak initialized");
 });
 
+const handleLogout = async function() {
+    console.log("Logging out...");
+    if (keycloak.authenticated) {
+        await keycloak.logout();
+    }
+}
 
 export default function Layout({children}: { children: React.ReactNode }) {
+
+    const containerStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%'
+    };
 
     return (
         <div>
@@ -31,9 +53,10 @@ export default function Layout({children}: { children: React.ReactNode }) {
                 </Scroller>
 
                 <div slot="navbar">
-                    <div style={{display: 'flex', alignItems: 'center'}}>
+                    <div style={containerStyle}>
                         <DrawerToggle/>
                         <h2>Orders</h2>
+                        <Button theme="secondary" onClick={handleLogout}>Logout</Button>
                     </div>
                 </div>
 
