e15399447e3c1f0f194b586b7839f2d453695dd4
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-02-23 04:24:14 +0100

    chore: fixing rule build-api-dev-ghcr

--- .make/docker-build.mk
+++ .make/docker-build.mk
@@ -25,4 +25,4 @@ build-admin-dev: install-docker admin-install
 	COMPOSE_PROFILES=backend_admin_dev docker-compose --env-file env/env.properties -f dc-local.yml build
 
 build-api-dev-ghcr: install-no-env
-	docker build -t ghcr.io/lsdaf/lsdaf_api-dev:latest -f docker/Dockerfile-dev . --build-arg module_folder=lsadf_api
+	docker build -t ghcr.io/lsdaf/lsadf_api-dev:latest -f docker/Dockerfile-dev . --build-arg module_folder=lsadf_api
